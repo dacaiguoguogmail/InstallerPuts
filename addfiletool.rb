d49e1900db25b3e85fa02324289c0eebaed3a1db
dacaiguoguo <dacaiguoguo@gmail.com>
2017-07-02 15:28:19 +0800

    移除有点问题

--- addfiletool.rb
+++ addfiletool.rb
@@ -52,12 +52,41 @@ class AddFileTool
     end
     build_phase.add_file_reference(ardd, true)
   end
+
+  def add_copy_file(file_path, group_name, target_name)
+    # 找到target
+    target = proj_obj.targets.find { |e| e.name == target_name }
+    # 找到group_name所在的group，用的[]方式找，找到后再调用[group_name]，就拿到了要添加文件的group
+    to_add_group = (proj_obj.groups.find { |e| e[group_name]})[group_name]
+    # 找到所有的real_path是file_path的，select方法返回的是数组
+    find_file_array = to_add_group.files.select { |e| e.real_path.to_s == file_path }
+    # 对所有的已经在工程里的文件，进行remove操作，保证不重复添加
+    find_file_array.each do |e|
+      to_add_group.remove_reference(e)
+    end
+    # target.build_phases.each { |e| puts e.class }
+    # 找到build_phase
+    resources_build_phase = target.build_phases.find { |e| e.is_a?(Xcodeproj::Project::Object::PBXResourcesBuildPhase)}
+    # puts resources_build_phase
+    # # # 在build_phase里添加文件的同时，把文件加到对应的group里面
+    ardd = to_add_group.new_reference(file_path)
+    # # # build file 去重，如果有，先移除再添加
+    if resources_build_phase.files_references.include?(ardd)
+      puts 'to remove'
+      resources_build_phase.remove_file_reference(ardd)
+    end
+    resources_build_phase.add_file_reference(ardd, true)
+  end
+
 end
 
 loger = AddFileTool.new('/Users/sunyanguo/Documents/gitplace/InstallerPuts/InstallerPuts.xcodeproj')
 to_add_path = '/Users/sunyanguo/Documents/gitplace/InstallerPuts/installer3.yaml'
 to_add_build_path = '/Users/sunyanguo/Documents/gitplace/InstallerPuts/installer2.yaml'
-loger.add_file(to_add_path, 'VC')
+to_add_copy_path = '/Users/sunyanguo/Documents/gitplace/InstallerPuts/shan.png'
+# loger.add_file(to_add_path, 'VC')
 loger.add_build_file(to_add_build_path, 'VC', 'InstallerPuts')
+# loger.add_copy_file(to_add_copy_path, 'VC', 'InstallerPuts')
 loger.proj_obj.save
+
 
